ffdae17002cb3f95a97e716051fc9e247fce3474
Birgit Bartenschlager <birgit.bartenschlager@web.de>
2023-04-24 14:45:13 +0200

    implement HTML structure

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from "../images/logo.jpg";
+
+function Header() {
+  return (
+    <header>
+      <img src={logo} alt="little lemon logo" width="150px"></img>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Main() {
+  return <main>Main Section</main>;
+}
+
+export default Main;
