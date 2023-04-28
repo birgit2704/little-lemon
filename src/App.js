62589c70f7927c12aa886e7081b62f1c31a1c68a
Birgit Bartenschlager <birgit.bartenschlager@web.de>
2023-04-28 23:49:13 +0200

    implement footer

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <>
       <Header />
       <Main />
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,23 +1,37 @@
 import React from "react";
 import logo from "../../assets/images/logo.jpg";
+import { FaLinkedin, FaFacebook, FaTwitter } from "react-icons/fa";
 
 function Footer() {
   return (
-    <footer>
-      <img alt=""></img>
-      <ul>
-        <li>
-          <icon>Social Media 1</icon>
-        </li>
-        <li>
-          <icon>Social Media 2</icon>
-        </li>
-        <li>
-          <icon>Social Media 3</icon>
-        </li>
-      </ul>
-      <address>contact info</address>
-      <img alt="logo" src={logo} width="80px" />
+    <footer className="footer">
+      <div className="footer-container">
+        <ul className="footer-social">
+          <li>
+            <a href="#home">
+              <FaLinkedin className="social" />
+            </a>
+          </li>
+          <li>
+            <a href="#home">
+              <FaFacebook className="social" />
+            </a>
+          </li>
+          <li>
+            <a href="#home">
+              <FaTwitter className="social" />
+            </a>
+          </li>
+        </ul>
+        <address className="address">
+          Little Lemon
+          <br />
+          1234, Main Street
+          <br />
+          CHICAGO
+        </address>
+        <img className="footer-logo" alt="logo" src={logo} />
+      </div>
     </footer>
   );
 }
