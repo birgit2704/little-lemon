42effee5d79b46f9d684f140d798107d4b5381c6
Birgit Bartenschlager <birgit.bartenschlager@web.de>
2023-04-28 18:08:48 +0200

    implement specials

--- src/components/Homepage/SpecialCard.js
+++ src/components/Homepage/SpecialCard.js
@@ -1,22 +1,20 @@
-import React from "react";
-import lemondessert from "../../assets/images/lemon dessert.jpg";
-
 const SpecialCard = (props) => {
   return (
     <div className="card-container">
       <div className="image-container">
-        <img src={lemondessert} alt="lemon cake" />
+        <img
+          className="card-image"
+          src={require(`../../assets/images/${props.image}`)}
+          alt={props.title}
+        />
       </div>
       <div className="card-text-container">
         <div className="card-first-line">
-          <h3 className="card-title">Lemon Dessert</h3>
-          <p className="card-price">$5.00</p>
+          <h3 className="card-title">{props.title}</h3>
+          <p className="card-price">{props.price}</p>
         </div>
 
-        <p className="card-description">
-          This comes straight from grandma's recipe book, every last ingredient
-          has been sourced and is as authentic as can be imagined.
-        </p>
+        <p className="card-description">{props.description}</p>
         <p className="card-order">Order a delivery</p>
       </div>
     </div>
