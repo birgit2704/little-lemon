cfed47d87f29f86c7d34ffa7aabcd5b74dc945dc
Birgit Bartenschlager <birgit.bartenschlager@web.de>
2023-04-29 17:08:18 +0200

    create mobile view

--- src/components/Header/Nav.js
+++ src/components/Header/Nav.js
@@ -3,29 +3,29 @@ import { FaBars } from "react-icons/fa";
 
 function Nav() {
   return (
-    <ul className="navbar">
-      <li>
-        <a href="#home">Home</a>
-      </li>
-      <li>
-        <a href="#about">About</a>
-      </li>
-      <li>
-        <a href="#menu">Menu</a>
-      </li>
-      <li>
-        <a href="#reservations">Reservations</a>
-      </li>
-      <li>
-        <a href="#">Order Online</a>
-      </li>
-      <li>
-        <a href="#">Login</a>
-      </li>
-      <li>
-        <FaBars className="hamburger" onClick={() => console.log("clicked")} />
-      </li>
-    </ul>
+    <div>
+      <ul className="navbar">
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#menu">Menu</a>
+        </li>
+        <li>
+          <a href="#reservations">Reservations</a>
+        </li>
+        <li>
+          <a href="#">Order Online</a>
+        </li>
+        <li>
+          <a href="#">Login</a>
+        </li>
+      </ul>
+      <FaBars className="hamburger" onClick={() => console.log("clicked")} />
+    </div>
   );
 }
 
