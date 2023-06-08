a64d4c2a1d11f41e322bcb87dba2016b97e37262
Birgit Bartenschlager <birgit.bartenschlager@web.de>
2023-06-08 12:20:11 +0200

    fetch data

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,24 +2,21 @@ import React, { createContext, useReducer } from "react";
 import Header from "./Header/Header";
 import { Outlet } from "react-router-dom";
 import Footer from "./Footer/Footer";
+import fetchData, { fetchAPI } from "../fetchData";
 
 export const TimeContext = createContext({});
 
-const Main = () => {
-  const timeReducer = (times, action) => {
-    if (action.type === "UpdateTimes") return action.time;
-    if (action.type === "InitializeTimes") return times;
-    return times;
-  };
+const timeReducer = (times, action) => {
+  if (action.type === "UpdateTimes")
+    return { times: fetchAPI().then((res) => res.json()) };
+  if (action.type === "InitializeTimes")
+    return { times: fetchAPI().then((res) => res.json()) };
+};
 
-  const [times, dispatch] = useReducer(timeReducer, [
-    "17:00",
-    "18:00",
-    "19:00",
-    "20:00",
-    "21:00",
-    "22:00",
-  ]);
+const Main = () => {
+  const initialState = ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"];
+
+  const [times, dispatch] = useReducer(timeReducer, initialState);
 
   return (
     <TimeContext.Provider value={{ times, dispatch }}>
