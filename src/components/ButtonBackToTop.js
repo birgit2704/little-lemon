ea4dd19c628ffb9ce1d2bdd494c2fcb70a297f33
Birgit Bartenschlager <birgit.bartenschlager@web.de>
2023-06-09 18:16:09 +0200

    improve back to top button

--- src/components/ButtonBackToTop.js
+++ src/components/ButtonBackToTop.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { BsFillArrowUpCircleFill } from "react-icons/bs";
 
 const ButtonBackToTop = () => {
+  const [showButton, setShowButton] = useState();
+
+  useEffect(() => {
+    const handleScrollButtonVisibility = () => {
+      window.scrollY > 200 ? setShowButton(true) : setShowButton(false);
+    };
+    window.addEventListener("scroll", handleScrollButtonVisibility);
+
+    return () => {
+      window.removeEventListener("scroll", handleScrollButtonVisibility);
+    };
+  }, []);
+
   return (
-    <a className="backToTop" href="#header">
-      Back to top
-    </a>
+    <>
+      {showButton && (
+        <a className="backToTop" href="#header">
+          <BsFillArrowUpCircleFill /> Back to top
+        </a>
+      )}
+    </>
   );
 };
 
