f9250ec0f0a46a89faf07f0c693868647fc2329f
Birgit Bartenschlager <birgit.bartenschlager@web.de>
2023-06-09 15:40:44 +0200

    links for socials on footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,17 +8,17 @@ function Footer() {
       <div className="footer-container">
         <ul aria-label="list of social links" className="footer-social">
           <li>
-            <a href="#home">
+            <a href="https://linkedin.com" target="_blank">
               <FaLinkedin aria-label="linked-in" className="social" />
             </a>
           </li>
           <li>
-            <a href="#home">
+            <a href="https://facebook.com" target="_blank">
               <FaFacebook aria-label="facebook" className="social" />
             </a>
           </li>
           <li>
-            <a href="#home">
+            <a href="https://twitter.com" target="_blank">
               <FaTwitter aria-label="twitter" className="social" />
             </a>
           </li>
